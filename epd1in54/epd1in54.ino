ff20980b5ebab1aadce48434b53aa185fcfd7d90
Kent <commpower15@gmail.com>
2025-02-14 15:21:19 -0500

    IMU support working

--- epd1in54/epd1in54.ino
+++ epd1in54/epd1in54.ino
@@ -1,6 +1,10 @@
 #include <GxEPD2_BW.h>
 #include <Adafruit_GFX.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBold24pt7b.h>
+#include <ArduinoBLE.h>
+#include <Arduino_LSM6DS3.h>
+
 #define EPD_CS    10
 #define EPD_DC    9
 #define EPD_RST   8
@@ -8,6 +12,7 @@
 
 // Try different drivers: GxEPD2_154_D67, GxEPD2_154_T8
 GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
+boolean displayed = false;
 
 void waitWhileBusy() {
   Serial.print("Waiting for BUSY...");
@@ -21,22 +26,52 @@ void setup() {
   Serial.begin(9600);
   while(!Serial);
   pinMode(EPD_BUSY, INPUT);
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
   display.init(115200);
 
-  Serial.println("Forcing full refresh...");
-  display.setFullWindow();
-  display.firstPage();
-  do {
-    display.fillScreen(GxEPD_WHITE);
-    display.setTextColor(GxEPD_BLACK);
-    display.setFont(&FreeMonoBold9pt7b);  // Change font as needed
-    display.setCursor(10, 50);  // X=10, Y=50
-    display.print("Hello, E-Ink!");
-  } while (display.nextPage());
+  Serial.println("Starting one-shot display");
+  display.setRotation(2);  // 2 = 180-degree rotation
 
-  display.powerOff();
+  Serial.println("Displaying text...");
+  display.setFullWindow();
+  display.fillScreen(GxEPD_WHITE);  // White background
+
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(&FreeMonoBold24pt7b);
+  display.setCursor(10, 50);  // X=10, Y=50
+  display.print("I'm \nfucking\n him");
+
+  display.display();  // Only updates once
+  // display.powerOff(); // Saves power
 }
 
 void loop() {
+  if (!displayed) {
   // Nothing needed in loop
+    display.fillScreen(GxEPD_WHITE);  // White background
+
+    display.setTextColor(GxEPD_BLACK);
+    display.setFont(&FreeMonoBold24pt7b);
+    display.setCursor(10, 50);  // X=10, Y=50
+    display.print("Barley-coded");
+    display.display();
+    displayed = true;
+  }
+  float x, y, z;
+
+    if (IMU.accelerationAvailable()) {
+      IMU.readAcceleration(x, y, z);
+  
+      if (x > .5 || y > .5 || z > 1.5) {
+        Serial.println("lift detected");
+        digitalWrite(LED_BUILTIN, HIGH);
+        displayed = false;
+      }
+  
+    }
 }
