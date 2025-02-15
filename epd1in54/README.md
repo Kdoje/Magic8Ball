0c293f8db2531613b1469f1c6e10bdd3325879d6
Kent <commpower15@gmail.com>
2025-02-15 00:23:12 -0500

    Finalizes readme and formatting for 1.0 release

--- epd1in54/README.md
+++ epd1in54/README.md
@@ -1,3 +1,13 @@
 ### Important details:
 1. Consult phone pictures for wiring, 
 2. The system uses 3.3V for power -- we can use the battery to power this as a result
+
+### To build
+* build and deploy the `epd1in54.ino` to the IoT nano 33 with Inland 1.54" eink display connected to SPI to get the different phrases generating
+* Send data to the exposed Bluetooth endpoint using LightBlue
+    * Go to Peripherals
+    * Connect to `The8Ball`
+    * Write to the Service ending in `...9b34fb`
+    * Write a UTF-8 string value using `|` to separate entries. For example
+    `Open your third eye|consider purchasing dominos|Go to Jail. Do not pass go, Do not collect $200`
+    * Then shake the ball to observe one of your entries appear
--- epd1in54/epd1in54.ino
+++ epd1in54/epd1in54.ino
@@ -2,21 +2,44 @@
 #include <Adafruit_GFX.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
 #include <Fonts/FreeMonoBold24pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
 #include <ArduinoBLE.h>
 #include <Arduino_LSM6DS3.h>
+
 
 #define EPD_CS    10
 #define EPD_DC    9
 #define EPD_RST   8
 #define EPD_BUSY  7
 BLEService textService("180C");  // Custom service UUID
-BLECharacteristic textCharacteristic("2A56", BLEWrite | BLEWriteWithoutResponse, 600); // Max 50 chars
+BLECharacteristic textCharacteristic("2A56", BLEWrite | BLEWriteWithoutResponse, 3000);
 
 
 // Try different drivers: GxEPD2_154_D67, GxEPD2_154_T8
 GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY));
 boolean displayed = false;
-String strings[20];
+String phrases[20] = {
+    "You can't get mad at a guy for something he did in his early 30s",
+    "america has an opioid crisis and you are leveraging this for your antics",
+    "I'm f***ing embarrassed to be an indie game dev",
+    "another day volunteering at the betsy ross museum. everyone keeps asking me if they can fuck the flag.",
+    "vaulting my big ass over the tables at the vancouver homeowners association",
+    "What's the last 4 digits of your social security number?",
+    "I don't like being on planet earth anymore I rather be up in outer space, in my opinion.",
+    "I was into the glowjng people and the wiggly enemies and weird sand and then the game said glue king. I'm game ended",
+    "anybody else love they self? like I really f*** with me heavy",
+    "Peach had to of thrown at least one chair.... right?",
+    "Lmfao scott w the idea that society should function and you all should be kind to one another is crazyyyyy",
+    "It’s just a bad janny, not a bad week",
+    "Look yeah I hate yoshi",
+    "Developers are indispensable for this service. But this service must be designed w/o including them",
+    "I dont like taking a sip of house medicine and the first shot is a small splash in the mouth.",
+    "oh, youre here from tik tok?  (chuckle's lightly ) Is that like reddit for babies",
+    "i wish mr bean could talk so he could say some shit like \"OK that was pretty messed up.\"",
+    "did it hurt when you fell from heaven and kissed my stupid ass you mother fucker",
+    "i shou;ld be allowed on the roof of any building",
+    "i dont care what Yankee Doodle did in town. His toxic fanbase tells me everything."
+};
 int currentItemCount = 20;
 
 void waitWhileBusy() {
@@ -27,14 +50,9 @@ void waitWhileBusy() {
   Serial.println("Done.");
 }
 
+
 void setup() {
   Serial.begin(9600);
-  strings[0] = "Apple";
-  strings[1] = "Banana";
-
-  for (int i = 2; i < sizeof(strings) / sizeof(strings[0]); i++) {
-    strings[i] = "Uninit" + String(i);
-  }
 
  // while(!Serial);
   pinMode(EPD_BUSY, INPUT);
@@ -49,7 +67,7 @@ void setup() {
     while (1);
   }
 
-  BLE.setLocalName("Nano33IoT");  // Bluetooth name
+  BLE.setLocalName("The8Ball");  // Bluetooth name
   BLE.setAdvertisedService(textService);
   textService.addCharacteristic(textCharacteristic);
   BLE.addService(textService);
@@ -68,51 +86,82 @@ void setup() {
 
   Serial.println("Displaying text...");
   display.setFullWindow();
-  display.fillScreen(GxEPD_WHITE);  // White background
+  display.fillScreen(GxEPD_BLACK);  // White background
 
-  display.setTextColor(GxEPD_BLACK);
+  display.setTextColor(GxEPD_WHITE);
   display.setFont(&FreeMonoBold24pt7b);
   display.setCursor(10, 50);  // X=10, Y=50
-  display.print("I'm \nfucking\n him");
+  display.print("8-Ball\nloading\n...");
 
   display.display();  // Only updates once
   // display.powerOff(); // Saves power
+}
+
+String formatString(String input, int maxLineLength = 14) {
+    String output = "";
+    int lineLength = 0;
+
+    for (int i = 0; i < input.length(); i++) {
+        if (input[i] == ' ') {
+            // Find next word length
+            int nextWordLength = 0;
+            int j = i + 1;
+            while (j < input.length() && input[j] != ' ') {
+                nextWordLength++;
+                j++;
+            }
+
+            // If adding the next word exceeds the max length, insert a newline
+            if (lineLength + nextWordLength + 1 > maxLineLength) {
+                output += '\n';
+                lineLength = 0;
+            } else {
+                output += ' ';
+                lineLength++;
+            }
+        } else {
+            output += input[i];
+            lineLength++;
+        }
+    }
+
+    return output;
 }
 
 void drawCenterString(String buf) {
     int16_t x1, y1;
     uint16_t textWidth, textHeight;
     display.getTextBounds(buf, 0, 0, &x1, &y1, &textWidth, &textHeight); //calc width of new string
-    display.setCursor(0, (200-textHeight)/2+30);
+    display.setCursor(0, (200-textHeight)/2+5);
    //display.setCursor(50, 25);
-    display.print(buf);
+    display.print(formatString(buf));
 }
 
 void parseEntries(String input) {
-    const int MAX_ITEMS = sizeof(strings) / sizeof(strings[0]);
-    const char DELIMITER = ',';
+    const int MAX_ITEMS = sizeof(phrases) / sizeof(phrases[0]);
+    const char DELIMITER = '|';
     int count = 0;
     int startIndex = 0;
     currentItemCount = 0;
     int endIndex = input.indexOf(DELIMITER);
 
-    for (int i = 0; i < sizeof(strings) / sizeof(strings[0]); i++) {
-      strings[i] = "";
+    for (int i = 0; i < sizeof(phrases) / sizeof(phrases[0]); i++) {
+      phrases[i] = "";
     }
 
     while (endIndex != -1 && count < MAX_ITEMS) {
-          strings[count++] = input.substring(startIndex, endIndex);  // Extract substring
+          phrases[count++] = input.substring(startIndex, endIndex);  // Extract substring
           startIndex = endIndex + 1;  // Move start past the delimiter
           endIndex = input.indexOf(DELIMITER, startIndex);  // Find next delimiter
     }
 
     // Add the last part (or full string if no delimiter found)
     if (startIndex < input.length() && count < MAX_ITEMS) {
-        strings[count++] = input.substring(startIndex);
+        phrases[count++] = input.substring(startIndex);
     }
-    Serial.println("Strings parsed are:");
-    for (int i = 0; i < sizeof(strings) / sizeof(strings[0]); i++) {
-      Serial.print(strings[i]);
+    Serial.println("phrases parsed are:");
+    for (int i = 0; i < sizeof(phrases) / sizeof(phrases[0]); i++) {
+      Serial.print(phrases[i]);
       Serial.print(";;");
     }
     Serial.println();
@@ -124,7 +173,8 @@ void loop() {
 
   if (central.connected()) {  // Stay connected
     if (textCharacteristic.written()) {  // If data is received
-      char receivedText[600] = {0}; // Buffer for incoming text
+      Serial.println("Received data");
+      char receivedText[3000] = {0}; // Buffer for incoming text
       textCharacteristic.readValue(receivedText, sizeof(receivedText));
       parseEntries(receivedText);
       Serial.println(receivedText);
@@ -132,14 +182,14 @@ void loop() {
   }
   if (!displayed) {
   // Nothing needed in loop
-    display.fillScreen(GxEPD_WHITE);  // White background
+    display.fillScreen(GxEPD_BLACK);  // White background
 
     // display.setTextColor(GxEPD_WHITE);
-    display.setFont(&FreeMonoBold24pt7b);
+    display.setFont(&FreeMonoBold12pt7b);
     //display.setCursor(10, 50);  // X=10, Y=50
     int randomIndex = random(0, currentItemCount);
-    // display.print(strings[0]);
-    drawCenterString(strings[randomIndex]);
+    // display.print(phrases[0]);
+    drawCenterString(phrases[randomIndex]);
     display.display();
     displayed = true;
     display.powerOff();
@@ -149,7 +199,7 @@ void loop() {
   if (IMU.accelerationAvailable()) {
     IMU.readAcceleration(x, y, z);
 
-    if (x > .5 || y > .5 || z > 1.5) {
+    if (x > 1.5 || y > 1.5 || z > 1.5) {
       Serial.println("lift detected");
       digitalWrite(LED_BUILTIN, HIGH);
       displayed = false;
